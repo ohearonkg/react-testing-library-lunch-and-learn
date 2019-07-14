638a8d052693376b9e67accaa182579bcd4a9cc4
KG O'Hearon <kg.ohearon@redspace.com>
2019-07-14 16:40:01 -0300

    making async.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import SignInForm from "./components/SignInForm";
 
+const saveUser = async ({ username, password }) => {
+  const { data } = await axios.post(
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+  return data;
+};
+
 const App = () => (
   <div>
     <title>Testing Example</title>
-    <SignInForm />
+    <SignInForm onSubmitFunction={saveUser} />
   </div>
 );
 
