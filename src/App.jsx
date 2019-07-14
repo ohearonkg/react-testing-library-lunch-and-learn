61c3f731303996a5b9ea99119f0c0db1a82b5a71
KG O'Hearon <kg.ohearon@redspace.com>
2019-07-14 16:40:35 -0300

    updated.

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,10 @@ import React from "react";
 import SignInForm from "./components/SignInForm";
 import axios from "axios";
 
-const saveUser = async ({ username, password }) => {
-  const { data } = await axios.post(
-    "https://jsonplaceholder.typicode.com/posts"
-  );
-  return data;
-};
-
 const App = () => (
   <div>
     <title>Testing Example</title>
-    <SignInForm onSubmitFunction={saveUser} />
+    <SignInForm />
   </div>
 );
 
