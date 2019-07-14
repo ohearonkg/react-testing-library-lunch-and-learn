35757a06c4a8085ff5f50c7d6f55adcc5190829b
KG O'Hearon <kg.ohearon@redspace.com>
2019-07-14 15:25:18 -0300

    two failing tests.

--- src/components/SignInForm/SignInForm.spec.jsx
+++ src/components/SignInForm/SignInForm.spec.jsx
@@ -1,3 +1,32 @@
 import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import SignInForm from "./";
 
-describe("Sign In Form", () => {});
+const usernameInputLabel = /username/i;
+const sampleUsername = "SAMPLEUSERNAME";
+
+const setup = () => render(<SignInForm />);
+
+describe("Sign In Form", () => {
+  /**
+   * Finding Username Field
+   */
+  it("Should display the username field", () => {
+    const { getByLabelText } = setup();
+    expect(getByLabelText(usernameInputLabel));
+  });
+
+  /**
+   * Enter Text Into Username Field
+   */
+  it("Should allow the user to enter their username", () => {
+    const { getByLabelText, getByValue } = setup();
+    const usernameInput = getByLabelText(usernameInput);
+
+    // Enter Text Into Userename Field
+    fireEvent.change(usernameInput, { target: { value: sampleUsername } });
+
+    // Expect text to be displayed
+    expect(getByValue(sampleUsername));
+  });
+});
