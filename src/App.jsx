45ede58615aaf5f8815e6cd2cc98ec0e9fc74391
KG O'Hearon <kg.ohearon@redspace.com>
2019-07-14 14:35:19 -0300

    initial commit.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import SignInForm from "./components/SignInForm";
+
+const App = () => (
+  <div>
+    <title>Testing Example</title>
+    <SignInForm />
+  </div>
+);
+
+export default App;
---
+++ src/components/SignInForm/index.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const SignInForm = () => <div> Sign In Form</div>;
+
+export default SignInForm;
