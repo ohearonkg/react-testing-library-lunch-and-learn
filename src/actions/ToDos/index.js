be7861b618af3d180a6f53c81d63e5a22bef3e9e
KG O'Hearon <kg.ohearon@redspace.com>
2019-07-16 22:18:22 -0300

    now with all tests passing.

--- src/actions/ToDos/index.js
+++ src/actions/ToDos/index.js
@@ -1,3 +1,21 @@
-export const addToDo = () => {};
-export const removeToDo = () => {};
-export const toggleToDo = () => {};
+import { ADD_TO_DO, REMOVE_TO_DO, TOGGLE_TO_DO } from "./constants";
+
+export const addToDo = ({ id, text }) => ({
+  type: ADD_TO_DO,
+  payload: {
+    id,
+    text
+  }
+});
+
+export const removeToDo = ({ id }) => ({
+  type: REMOVE_TO_DO,
+  payload: { id }
+});
+
+export const toggleToDo = ({ id }) => ({
+  type: TOGGLE_TO_DO,
+  payload: {
+    id
+  }
+});
--- src/reducers/ToDos/index.js
+++ src/reducers/ToDos/index.js
@@ -11,8 +11,30 @@ const initialState = {
 const toDosReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_TO_DO:
+      const { id, text } = action.payload;
+      return {
+        toDos: [...state.toDos, { id, text, completed: false }]
+      };
     case REMOVE_TO_DO:
+      const indexToBeRemoved = state.toDos.findIndex(
+        ({ id }) => id === action.payload.id
+      );
+      const toDosCopy = [...state.toDos];
+      toDosCopy.splice(indexToBeRemoved, 1);
+      return {
+        toDos: toDosCopy
+      };
     case TOGGLE_TO_DO:
+      const {
+        payload: { id: idToToggle }
+      } = action;
+      return {
+        toDos: state.toDos.map(({ id, text, completed }) =>
+          id === idToToggle
+            ? { id, text, completed: !completed }
+            : { id, text, completed }
+        )
+      };
     default:
       return state;
   }
